4e0dfce2804decb0a5f315a2a7a630a096d202c8
Pawan Somavarpu <pawansomavarpu@Pawans-MacBook-Pro-2.local>
2024-03-30 18:25:22 -0500

    skeleton structure made

--- senior-access-tool/src/App.js
+++ senior-access-tool/src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+      
+        <h1 className="font-bold">Better Unite Conversion Tool</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        
+        
+        <Stack spacing={2} direction={"row"} alignItems="center" justifyContent="center">
+          
+          <Button href="#new" variant="contained">Create New CSV</Button>
+          <Button href="#add" variant="contained">Add To Existing CSV</Button>
+
+        </Stack>
+
+        
+        
+        <Container id="new">
+          <h2>Create New CSV</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <Form />
+        </Container>
+        
+        <Container id="add">
+          <h2 >Add To Existing CSV</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <Form />
+
+        </Container>
+        
+       
+      </Container>
+
+      
     </div>
   );
 }
---
+++ senior-access-tool/src/components/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div><p>This is inside the Form Component.</p></div>
+  )
+}
+
+export default Form
