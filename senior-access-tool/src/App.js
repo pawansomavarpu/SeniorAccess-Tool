8d0fe181c09d66968309df29c21f815021aab6ab
Kaycie Doan <kayciedoan@gmail.com>
2024-04-02 14:39:37 -0500

    info blurbs

--- senior-access-tool/src/App.js
+++ senior-access-tool/src/App.js
@@ -10,8 +10,9 @@ function App() {
     <div className="App">
       <Container>
       
-        <h1 className="font-bold">Better Unite Conversion Tool</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h1 className="font-bold">BetterUnite Conversion Tool</h1>
+        <p>This tool allows you to organize all of your contacts into files that are ready to import into BetterUnite. 
+          <br /><br /><b>Please choose one option:</b></p>
         
         
         <Stack spacing={2} direction={"row"} alignItems="center" justifyContent="center">
@@ -25,17 +26,24 @@ function App() {
         
         <Container id="new">
           <h2>Create New CSV</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>1. Fill out the information for a new contact. 
+          <br />2. When you are done, press "Add New Contact" to add it to the file. 
+          <br /> 3. Repeat these steps for the next contact. 
+          <br />
+            <br />When you have finished adding all contacts, press "Generate CSV" to review and download your file.</p>
           <Form />
         </Container>
         
         <Container id="add">
           <h2 >Add To Existing CSV</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>1. Fill out the information for a new contact. 
+          <br />2. When you are done, press "Add New Contact" to add it to the file. 
+          <br /> 3. Repeat these steps for the next contact. 
+          <br />
+            <br />When you have finished adding all contacts, press "Generate CSV" to review and download your file.</p>
           <Form />
 
         </Container>
-        
        
       </Container>
 
